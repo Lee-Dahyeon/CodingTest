d477c69bc415ada964e92712ace6415625da840b
이다현 <146173751+Lee-Dahyeon@users.noreply.github.com>
2024-08-25 17:11:40 +0900

    [Silver IV] Title: ATM, Time: 212 ms, Memory: 20832 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,26 +1,27 @@
-import java.util.Scanner;
+
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] p = new int[n];
+        //받은 숫자 배열 생성
         for (int i = 0; i < n; i++) {
             p[i] = sc.nextInt();
         }
-        int[] sum = new int[n];
-        int result =0;
+        int temp = 0;//누적합
+        int result =0; //최종 시간
+
+        //오름차순 정리
         Arrays.sort(p);
 
+        //기다리는 시간 계산
         for (int i = 0; i < n; i++) {
-            if(i==0){
-                sum[i]=p[i];
-                result=sum[i];
-            }else {
-                sum[i] = sum[i - 1] + p[i];
-                result+=sum[i];
-            }
+            temp += p[i];
+            result+=temp;
+
         }
         System.out.println(result);
     }
