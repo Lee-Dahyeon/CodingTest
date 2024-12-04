ea55636ec5549f92166c3fda9263ae2adfbeacca
이다현 <146173751+Lee-Dahyeon@users.noreply.github.com>
2024-12-04 17:43:10 +0900

    [Gold IV] Title: 떡장수와 호랑이, Time: 324 ms, Memory: 27000 KB -BaekjoonHub

--- 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.java
+++ 백준/Gold/16432. 떡장수와 호랑이/떡장수와 호랑이.java
@@ -1,46 +1,48 @@
-import java.util.*;
- 
-public class Main {    
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
     static ArrayList<Integer>[] list;
     static boolean[][] visited;
     static int[] result;
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        
         int n = scan.nextInt();
-        
-        list = new ArrayList[n + 1];
-        for(int i = 1; i < n + 1; i++) {
-            list[i] = new ArrayList<>();
+        list = new ArrayList[n+1];
+
+        for (int i = 1; i < n+1; i++) {
+            list[i]=new ArrayList<>();
             int m = scan.nextInt();
-            for(int j = 0; j < m; j++) {
+            for (int j = 0; j < m; j++) {
                 list[i].add(scan.nextInt());
             }
         }
-        
+        visited = new boolean[n + 2][10];
         result = new int[n + 1];
-        visited = new boolean[n + 2][10]; 
+
         dfs(1, 0, n);
-        System.out.println("-1");
-        
-    }    
-    
+        System.out.println(-1);
+    }
+
     public static void dfs(int idx, int prev, int n) {
-        if(idx == n + 1) {
-            for(int i = 1; i < n + 1; i++) {
+        if (idx == n + 1) {
+            for (int i = 1; i < n+1; i++) {
                 System.out.println(result[i]);
             }
             System.exit(0);
         }
-    
-        for(int i = 1; i < 10; i++) {
-            if(i != prev && visited[idx + 1][i] == false && list[idx].contains((Integer)i)) {
-                visited[idx + 1][i] = true;
-                result[idx] = i;
-                dfs(idx + 1, i, n);
+        for (int i = 1; i<10; i++) {
+            if (i != prev && visited[idx+1][i]==false && list[idx].contains(i)) {
+                visited[idx+1][i] = true; 
+                result[idx] = i; 
+                dfs(idx+1, i, n);
             }
         }
-    }
+        }
 }
