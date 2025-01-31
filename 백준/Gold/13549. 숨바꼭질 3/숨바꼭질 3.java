fd33fc784c38753a4dc4bb00dc878082cdc5bc19
이다현 <146173751+Lee-Dahyeon@users.noreply.github.com>
2025-01-31 22:06:36 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 232 ms, Memory: 87072 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,40 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int max = 100000;
+    static int max=100000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        boolean[] visited=new boolean[max+1];
 
-        boolean[] visited = new boolean[max + 1];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{n, 0}); 
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{N, 0});
-
-        int answer = Integer.MAX_VALUE;
-
-        while (!queue.isEmpty()) {
-            int[] tmp = queue.poll();
+        int answer=Integer.MAX_VALUE;
+        while (!q.isEmpty()) {
+            int[] tmp = q.poll();
             int current = tmp[0]; 
-            int time = tmp[1];
+            int time= tmp[1];
             visited[current] = true;
 
-            if (current == K) {
-                answer = Math.min(answer, time);
+            if (current == k) { 
+                answer=Math.min(answer,time);
             }
-
-            if (current * 2 <= max && !visited[current * 2]) {
-                queue.offer(new int[]{current * 2, time});
+            if (current*2<= max && !visited[current*2]) {
+                q.add(new int[]{current*2, time});
             }
-            if (current + 1 <= max && !visited[current + 1]) {
-                queue.offer(new int[]{current + 1, time + 1});
+            if (current+1<= max && !visited[current+1]) {
+                q.add(new int[]{current+1, time+1});
             }
-            if (current - 1 >= 0 && !visited[current - 1]) {
-                queue.offer(new int[]{current - 1, time + 1});
+            if (current-1>=0 && !visited[current-1]) {
+                q.add(new int[]{current-1, time+1});
             }
         }
         System.out.println(answer);
