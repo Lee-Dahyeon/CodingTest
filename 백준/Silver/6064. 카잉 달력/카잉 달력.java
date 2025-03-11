7d3a7d1d65124faf3ebdb7aeeac7dfeb57d38398
이다현 <146173751+Lee-Dahyeon@users.noreply.github.com>
2025-03-12 01:21:42 +0900

    [Silver I] Title: 카잉 달력, Time: 248 ms, Memory: 17984 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.java
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -1,32 +1,48 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
+    static class Case{
+        int m;
+        int n;
+        int x;
+        int y;
+        Case(int m, int n, int x, int y){
+            this.m = m;
+            this.n = n;
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        ArrayList<Case> cases = new ArrayList<>();
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken())-1;
+            int y = Integer.parseInt(st.nextToken())-1;
+            cases.add(new Case(m,n,x,y));
+        }
+        for (Case c : cases) {
+            boolean check = false;
+            for (int i = c.x; i < (c.m*c.n); i+=c.m) {
+                if (i%c.n ==c.y) {
+                    System.out.println(i+1);
+                    check = true;
+                    break;
+                }
+            }
+            if (!check) {
+                System.out.println(-1);
+            }
+        }
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		
 
-		for (int s = 0; s < T; s++) {
-
-			boolean check = false;
-			int m = sc.nextInt();
-			int n = sc.nextInt();
-			int x = sc.nextInt() - 1;
-			int y = sc.nextInt() - 1;
-
-			for (int i = x; i < (n * m); i += m) {
-				if (i % n == y) {
-					System.out.println(i + 1);
-					check = true;
-					break;
-				}
-			}
-
-			if (!check) {
-				System.out.println(-1);
-
-			}
-		}
-	}
+    }
 }
